28ebb5770e731dc0dacf80d41aeed59582dfb6d2
swastik007 <swastik.nakarmi@gmail.com>
2024-07-26 18:00:37 +0545

    images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 import Header from './components/HeaderSection.jsx'
 import HeroSection from './components/HeroSection.jsx'
 function App() {
