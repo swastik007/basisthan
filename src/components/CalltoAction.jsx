ba3eadbcb3cc7f3075f07986258b44a33e4b65fb
swastik007 <swastik.nakarmi@gmail.com>
2024-09-06 15:21:15 +0545

    design changes

---
+++ src/components/CalltoAction.jsx
@@ -0,0 +1,25 @@
+const CalltoAction = () => {
+    return(
+    
+    <div className="container-fluid bg-[url('/assets/img/hero-pattern.svg')] bg-cover bg-center bg-no-repeat">
+            <div className="container">
+                <div className="flex justify-content-center">
+                    <div className="max-w-screen-xl mx-auto">
+                        <div className="text-center">
+                            <div className="text-center">
+                                <h3>Get started with our awesome service today</h3>
+                                <p>Productboard is the product management system that helps you understand what users need.</p>
+                            </div>
+                            <div className="flex align-items-center justify-content-center">
+                                <a href="contact.html" className="btn">Get Started Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+);
+}
+
+export default CalltoAction;
